7c0b496666f2481ac14f4506455e84a448d86f45
ewheeler <evanmwheeler@gmail.com>
2010-05-10 16:55:16 -0400

    fixed import, added related_name

--- apps/edusupply/extensions/rapidsms/contact.py
+++ apps/edusupply/extensions/rapidsms/contact.py
@@ -2,11 +2,11 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 from django.db import models
-from location import School
+from rapidsms.contrib.locations.models import Location
 
 class Headmaster(models.Model):
     alternate_phone = models.CharField(max_length=160, blank=True, null=True)
-    school = models.ForeignKey(School, blank=True, null=True)
+    school = models.ForeignKey(Location, blank=True, null=True, related_name='headmasters')
 
     class Meta:
         abstract = True
