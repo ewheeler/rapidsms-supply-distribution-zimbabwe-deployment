433bb6fde63f68eb59fd758a0f90ef53a9e10a52
ewheeler <evanmwheeler@gmail.com>
2010-05-10 16:56:34 -0400

    moved location extensions to proper directory

--- apps/edusupply/extensions/locations/location.py
+++ apps/edusupply/extensions/locations/location.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class School(models.Model):
     address = models.CharField(max_length=500, blank=True, null=True)
-    km_to_DEO = models.Charfield(max_length=160, blank=True, null=True)
+    km_to_DEO = models.CharField(max_length=160, blank=True, null=True)
     code = models.PositiveIntegerField(max_length=20, blank=True, null=True)
     satellite_id = models.PositiveIntegerField(max_length=20, blank=True, null=True)
     
