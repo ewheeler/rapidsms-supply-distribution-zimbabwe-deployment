9855f729ed350823eb99957ab22c6194ef6809e0
ewheeler <evanmwheeler@gmail.com>
2010-05-11 17:39:12 -0400

    renamed satellite_id field to satellite_number

--- apps/edusupply/extensions/locations/location.py
+++ apps/edusupply/extensions/locations/location.py
@@ -7,7 +7,7 @@ class School(models.Model):
     address = models.CharField(max_length=500, blank=True, null=True)
     km_to_DEO = models.CharField(max_length=160, blank=True, null=True)
     code = models.PositiveIntegerField(max_length=20, blank=True, null=True)
-    satellite_id = models.PositiveIntegerField(max_length=20, blank=True, null=True)
+    satellite_number = models.PositiveIntegerField(max_length=20, blank=True, null=True)
     
     class Meta:
         abstract = True
