d8ea84f69d7f75ff329c5feb0f877661450b844a
ewheeler <evanmwheeler@gmail.com>
2010-05-27 12:55:53 -0400

    improved logistics admin interface

--- apps/logistics/admin.py
+++ apps/logistics/admin.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 
 from django.contrib import admin
 from .models import *
 
+class CommodityAdmin(admin.ModelAdmin):
+    list_display = ("name", "aliases", "unit", "volume", "weight")
 
-admin.site.register(Commodity)
-admin.site.register(Cargo)
-admin.site.register(Shipment)
+class CargoAdmin(admin.ModelAdmin):
+    list_filter = ("commodity", )
+
+class ShipmentAdmin(admin.ModelAdmin):
+    list_display = ("cargos_str", "origin", "destination", "status", "shipping_time", "actual_delivery_time")
+    list_filter = ("status",)
+    date_hierarchy = "shipping_time"
+
+class ShipmentSightingAdmin(admin.ModelAdmin):
+    list_display = ("observed_cargo", "location", "seen_by", "updated")
+    search_fields = ("observed_cargo", "location", "seen_by", "updated")
+    date_hierarchy = "updated"
+
+admin.site.register(Commodity, CommodityAdmin)
+admin.site.register(Cargo, CargoAdmin)
+admin.site.register(Shipment, ShipmentAdmin)
 admin.site.register(ShipmentRoute)
-admin.site.register(ShipmentSighting)
+admin.site.register(ShipmentSighting, ShipmentSightingAdmin)
