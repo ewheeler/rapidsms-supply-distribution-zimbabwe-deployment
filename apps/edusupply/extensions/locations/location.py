122be9fb05cbd3d4ae7cc0400f0f81caec4c6a38
ewheeler <evanmwheeler@gmail.com>
2010-05-27 13:25:52 -0400

    helper properties for web display

--- apps/edusupply/extensions/locations/location.py
+++ apps/edusupply/extensions/locations/location.py
@@ -14,6 +14,51 @@ class School(models.Model):
     class Meta:
         abstract = True
 
+    @property
+    def full_code(self):
+        return "%s" % (str(self.code) + str(self.satellite_number))
+
+    @property
+    def contact(self):
+        contacts = self.facilitycontact.all()
+        if contacts.count() == 1:
+            return contacts[0]
+        if contacts.count() == 0:
+            return ""
+        if contacts.count() > 0:
+            return contacts.values_list('name', flat=True)
+
+    @property
+    def contact_phone(self):
+        contacts = self.facilitycontact.all()
+        if contacts.count() == 1:
+            connections = contacts[0].connection_set.all()
+            if connections.count() == 1:
+                return connections[0]
+            if connections.count() > 1:
+                return connections.values_list('identity', flat=True)
+            if contacts[0].phone is not None:
+                return contacts[0].phone
+            else:
+                return ""
+        if contacts.count() == 0:
+            return ""
+        if contacts.count() > 0:
+            return contacts.values_list('phone', flat=True)
+
+    @property
+    def contact_alt_phone(self):
+        contacts = self.facilitycontact.all()
+        if contacts.count() == 1:
+            if contacts[0].alternate_phone is not None:
+                return contacts[0].alternate_phone
+            else:
+                return ""
+        if contacts.count() == 0:
+            return ""
+        if contacts.count() > 0:
+            return contacts.values_list('alternate_phone', flat=True)
+
     @classmethod
     def closest_by_code(klass, search_string, n=100):
         d = []
