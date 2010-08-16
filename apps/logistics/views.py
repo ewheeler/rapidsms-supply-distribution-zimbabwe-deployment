7b2115b186ec6e54ac58bcd4d14ed169152ea0cd
ewheeler <evanmwheeler@gmail.com>
2010-08-16 18:02:41 -0400

    remove old tag import

--- apps/logistics/views.py
+++ apps/logistics/views.py
@@ -2,7 +2,6 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
 
-from django.templatetags.tabs_tags import register_tab
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
